e91a4722daa678b0928c98a1497a15a36f267bbf
noteatingshit <78374970+noteatingshit@users.noreply.github.com>
2021-02-10 19:14:40 +0100

    Update cakefan.plugin.js

--- cakefan.plugin.js
+++ cakefan.plugin.js
@@ -4,7 +4,7 @@
  * @invite
  * @website
  * @source
- * @updateUrl https://raw.githubusercontent.com/noteatingshit/cakefan/main/cakefan.plugin.js?token=ASV6QOWUBUQGGUMWUV2WSODAEQKTQ
+ * @updateUrl https://raw.githubusercontent.com/noteatingshit/cakefan/main/cakefan.plugin.js?token=ASV6QOSOEUTEREOOL3XKTALAEQRGU
  */
 
 /*@cc_on
@@ -49,7 +49,7 @@ const config = {
     version: "0.0.1",
     description: "боже ты такая милая, я просто в ахуе",
     github: "",
-    github_raw: "https://raw.githubusercontent.com/noteatingshit/cakefan/main/cakefan.plugin.js?token=ASV6QOWUBUQGGUMWUV2WSODAEQKTQ",
+    github_raw: "https://raw.githubusercontent.com/noteatingshit/cakefan/main/cakefan.plugin.js?token=ASV6QOSOEUTEREOOL3XKTALAEQRGU",
     changelog: [
       {
         title: "New meta",
